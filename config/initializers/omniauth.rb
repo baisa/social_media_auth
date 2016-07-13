ddd66950315e222ae720e0032befe94c18193e9b
Barbara Slawinska <slawinskabarbara@gmail.com>
2016-07-13 14:32:09 +0100

    getting facebook settings and links

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'EnPDBBOmEHM27nDboiw56HRxu', 'FoK3KEWC6NEYYE1jFJgFsIV8Gn1XKD8FNLu4ezIhyGZiKXszKv'
+  provider :facebook, '119144325179684','a16cf49da993d266e672a82e10cb0b86'
+    scope: 'public_profile', info_fields: 'id,name,link'
 end
